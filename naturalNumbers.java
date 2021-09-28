cc5d175871a3ed2e50f2bf03a74a1c2e7921c2e7
Rohan-pyt <76887363+Rohan-pyt@users.noreply.github.com>
2021-09-28 12:15:36 +0530

    naturalno

---
+++ naturalNumbers.java
@@ -0,0 +1,16 @@
+package assing2;
+
+public class naturalNumbers {
+	public static void main(String[] args) {
+		 int n = 10, i, total = 0;
+
+	       for(i = 1; i <= n; i++){
+	           total =total + i;
+	       }
+
+	       System.out.println("Sum of first 10 natural numbers is: "+total);
+	    }
+	
+	}
+
+
